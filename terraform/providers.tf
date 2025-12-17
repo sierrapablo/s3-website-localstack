31472ddf0686b3ca88cc68e2257187f670efa385
Pablo Sierra Lorente <pablosierralorente@gmail.com>
2025-12-18 00:32:12 +0100

    add required versions and providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,13 @@
-# Provider configuration
-# Use versions.tf for version constraints
+provider "aws" {
+  region                      = var.aws_region
+  access_key                  = var.localstack_dummy
+  secret_key                  = var.localstack_dummy
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 
-provider "null" {
-  # Configuration options if needed
+  endpoints {
+    s3 = var.localstack_endpoint
+  }
 }
 
-# Example:
-# provider "aws" {
-#   region = "us-east-1"
-# }
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,19 +1,13 @@
-# Terraform version
-# Use this file to define minimum version of Terraform required to run this code.
-
 terraform {
-  required_version = ">= 1.13"
+  required_version = ">= 1.13.0"
 
   required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
 
-    # Example:
-    # aws = {
-    #   source  = "hashicorp/aws"
-    #   version = "~> 5.0"
-    # }
   }
+
+  backend "s3" {}
 }
